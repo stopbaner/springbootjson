252b262969b90bcf7dddc3ea7b44f52f71ab843c
stopbaner <97634674@qq.com>
2022-02-25 14:04:00 +0800

    update

--- exceltest/src/main/java/com/shenzp/dao/DemoDAO.java
+++ exceltest/src/main/java/com/shenzp/dao/DemoDAO.java
@@ -5,7 +5,7 @@ import com.shenzp.easyexcel.entity.ExcelDemo;
 import java.util.List;
 
 /**
- * 假设这个是你的DAO存储。当然还要这个类让spring管理，当然你不用需要存储，也不需要这个类。
+ * 假设这个是你的DAO存储。当然还要这个类让spring管理，当然你不用需要存储，也不需要这个类。excel数据存入数据库
  **/
 public class DemoDAO {
     public void save(List<ExcelDemo> list) {
